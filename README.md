d3433e1cfbed4334303b2801f55284148e1b3034
florian935 <florian.martin63000@gmail.com>
2022-02-07 18:20:44 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Spring Cloud with Cloud Karafka
+
+The goal of this project is to demonstrate how to implement Publisher / Subscriber with Kafka and Spring Cloud by using the Cloud Karafka provider.
---
+++ publisher/src/test/java/com/florian935/cloud/kafka/publisher/PublisherApplicationTests.java
@@ -0,0 +1,13 @@
+package com.florian935.cloud.kafka.publisher;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PublisherApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
